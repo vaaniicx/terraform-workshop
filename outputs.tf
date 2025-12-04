e1ccd8a1bb53e7ccf2e06db44275d806c6afb706
Yodirna <ridoy014@gmail.com>
2025-12-04 23:43:07 +0100

    outputs and provider

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "elb-dns-name" {
   description = "DNS Name of the Load Balancer"
   value       = aws_lb.lb.dns_name
 }
+
+# Zeigt die IP-Adresse des EC2-Instanzen an nach einem erfolgreichen Terraform Apply
+# Diese IP-Adresse kann verwendet werden, um auf die EC2-Instanz zuzugreifen
+# Die IP wird im Terminal angezeigt
--- provider.tf
+++ provider.tf
@@ -15,6 +15,15 @@ terraform {
   }
 }
 
+# Konfiguriert die Terraform-Einstellungen
+# Einschließlich der erforderlichen Anbieter (AWS) und der Terraform-Version
+# Mindestens Version 1.2 für Terraform erforderlich
+# Backend S3 Bucket wird verwendet um Terraform State zu speichern
+# Außerdem ist der S3 Bucket für GitHub Actions konfiguriert (z.B. Terraform Destroy)
+
 provider "aws" {
   region = var.aws_region
 }
+
+# Konfiguriert den AWS-Anbieter mit der angegebenen Region
+# Es ist möglich die Region in der Variable aws_region zu ändern
